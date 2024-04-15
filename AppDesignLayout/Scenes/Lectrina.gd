15f4e6bb623e65558e82ccfdc78a916e4ede5b9a
Varbil <jonas.bilgrav.pedersen@gmail.com>
2024-04-15 15:01:45 +0200

    Removed Scrolling Indicator and added navigation indicator script

--- AppDesignLayout/Scenes/Lectrina.gd
+++ AppDesignLayout/Scenes/Lectrina.gd
@@ -5,6 +5,12 @@ var Books : Control
 var Leaderboard : Control
 var Profile : Control
 
+var HomeNav : Button
+var TrainingNav : Button
+var BooksNav : Button
+var LeaderboardNav : Button
+var ProfileNav : Button
+
 func _ready():
 	# Load the references to the menu scenes
 	HomeScreen = get_node("HomeScreen")
@@ -13,9 +19,14 @@ func _ready():
 	Leaderboard = get_node("Leaderboard")
 	Profile = get_node("Profile")
 	
+	HomeNav = get_node("NavBar/HomeNav")
+	TrainingNav = get_node("NavBar/TrainingNav")
+	BooksNav = get_node("NavBar/BooksNav")
+	LeaderboardNav = get_node("NavBar/LeaderboardNav")
+	ProfileNav = get_node("NavBar/ProfileNav")
+	
 	# Initially hide all scenes except the home scene
-	hide_all_scenes()
-	HomeScreen.visible = true
+	_on_HomeNav_pressed()
 
 func hide_all_scenes():
 	HomeScreen.visible = false
@@ -23,23 +34,34 @@ func hide_all_scenes():
 	Books.visible = false
 	Leaderboard.visible = false
 	Profile.visible = false
+	
+	HomeNav.disabled = false
+	TrainingNav.disabled = false
+	BooksNav.disabled = false
+	LeaderboardNav.disabled = false
+	ProfileNav.disabled = false
 
 func _on_HomeNav_pressed():
 	hide_all_scenes()
+	HomeNav.disabled = true
 	HomeScreen.visible = true
 
 func _on_TrainingNav_pressed():
 	hide_all_scenes()
+	TrainingNav.disabled = true
 	Training.visible = true
 
 func _on_BooksNav_pressed():
 	hide_all_scenes()
+	BooksNav.disabled = true
 	Books.visible = true
 
 func _on_LeaderboardNav_pressed():
 	hide_all_scenes()
+	LeaderboardNav.disabled = true
 	Leaderboard.visible = true
 
 func _on_ProfileNav_pressed():
 	hide_all_scenes()
+	ProfileNav.disabled = true
 	Profile.visible = true
